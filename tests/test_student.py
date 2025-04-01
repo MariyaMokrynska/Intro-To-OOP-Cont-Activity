5887c81b59475fbe29282ee07d2de63491ef4bd0
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-01 15:32:20 -0700

    implemented 5 test functions

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,23 +1,22 @@
-import pytest
 from school_schedule.student import Student
-
+# Nominal cases
 def test_student_attributes():
-    #Assert
+    #Arrange
     name = "Marina"
     grade = "senior"
     classes = ["Precalculus", "Javascript"]
 
     #Act
-    student = Student(name, grade, classes)
+    student = Student(name, grade, classes) 
 
     #Assert
     assert student.name == name
     assert student.grade == grade
-    assert len(student.classes) == 2
+    assert student.get_num_classes() == 2
     assert "Precalculus" in student.classes
 
 def test_add_class_to_student():
-    #Assert
+    #Arrange
     name = "Mariya"
     grade = "junior"
     classes = ["Python", "PhP"]
@@ -28,25 +27,51 @@ def test_add_class_to_student():
     result = student.add_class(class_name)
 
     #Assert
-    assert len(classes) == 3
+    assert len(result) == 3
     assert class_name in student.classes
 
-def test_non_existing_attribute_raise_exception():
-    #Assert
+def test_summary_method():
+    # Arrange
+    name = "Andrew"
+    grade = "junior"
+    classes = ["Intro to Computer Science", "Public Speaking"]
+    student = Student(name, grade, classes)
+
+    # Act
+    result = student.summary()
+
+    # Assert
+    assert result == "Andrew is a junior enrolled in 2 classes: Intro to Computer Science, Public Speaking"
+
+# Edge cases
+# Based on the add_class() implementation, duplicates are NOT handled.
+def test_add_duplicate_class():
+    #Arrange
     name = "Anna"
     grade = "senior"
-    classes = ["Bilogy", "Chemistry", "Pharmacy"]
-    email = "xyz@gmail.com"
+    classes = ["Math", "Sql"]
+    student = Student(name, grade, classes)
 
-    with pytest.raises(TypeError, match="Student.__init__\(\) takes 4 positional arguments but 5 were given"):
-        Student(name, grade, classes, email)
- 
-def test_passing_not_all_arg_raises_exception():
+    #Act
+    result = student.add_class("Math")
+
     #Assert
+    assert len(result) == 3
+    assert "Math" in student.classes
+    assert result == ["Math", "Sql", "Math"]
+
+def test_return_zero_on_empty_class_list():
+    #Arrange
     name = "Mark"
     grade = "junior"
+    classes = []
+    student = Student(name, grade, classes)
 
-    with pytest.raises(TypeError, match="Student.__init__\(\) missing 1 required positional argument: 'classes'"):
-        Student(name, grade)
+    #Act
+    result = student.get_num_classes()
+
+    #Assert
+    assert result  == 0
+
 
 

baa2c332db4be74c61ee5ebc95e8d0bc91f3b9d0
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-01 12:09:48 -0700

    implemented 3 test functions

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,3 +1,42 @@
+import pytest
 from school_schedule.student import Student
 
-# Write your tests here!
+def test_student_attributes():
+    #Assert
+    name = "Marina"
+    grade = "senior"
+    classes = ["Precalculus", "Javascript"]
+
+    #Act
+    student = Student(name, grade, classes)
+
+    #Assert
+    assert student.name == name
+    assert student.grade == grade
+    assert len(student.classes) == 2
+    assert "Precalculus" in student.classes
+
+def test_add_class_to_student():
+    name = "Mariya"
+    grade = "junior"
+    classes = ["Python", "PhP"]
+    student = Student(name, grade, classes)
+
+    class_name = "Java"
+    result = student.add_class(class_name)
+
+    assert len(classes) == 3
+    assert class_name in student.classes
+
+def test_non_existing_attribute_raise_exception():
+    #Assert
+    name = "Anna"
+    grade = "senior"
+    classes = ["Bilogy", "Chemistry", "Pharmacy"]
+    email = "xyz@gmail.com"
+
+    with pytest.raises(TypeError, match="Student.__init__\(\) takes 4 positional arguments but 5 were given"):
+        Student(name, grade, classes, email)
+ 
+
+
